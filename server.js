41878a38f4bf9557ce59bfc9c59c9db587261834
Christopher Lyons <ChristopherLyons17@hotmail.com>
2019-07-26 10:53:01 -0600

    websocket

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
+var http = require("http");
 var PORT = 8080
 
 var server = http.createServer()
+const WebSocket = require('ws');
+var socket = new WebSocket("ws://localhost:8080");
+socket.on('connection', function(event) {
+  socket.send("Hello world!");
+});
 
-var sockjs = require('sockjs')
-var wss = sockjs.createServer()
-wss.on('connection', function(ws) {
-  ws.on('data', function(data) {
-    ws.write('from server: ' + data)
-  })
-  ws.on('close', function() {
-    console.log('close')
-  })
-})
-
-//wss.installHandlers(server)
 server.listen(PORT)
